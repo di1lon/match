876ca8868a13d933246bec65fab08ae90ae41d8c
di1lon <ferrovitreous@gmail.com>
2017-02-10 20:49:32 -0500

    sound and reset
    added sound and reset. now using vw as unit.

--- js/main.js
+++ js/main.js
@@ -3,17 +3,23 @@ var clickz = 0;
 var pair = [];
 var match = 0;
 
+var audio = new Audio('snd/prox.wav');
+var audio2 = new Audio('snd/beverly_computer.wav');
 
 
-//need fucntion to dynamically create content array depending on desired number of blocks
+
+
+
+
+
+function createBoard() {
+
+  //need fucntion to dynamically create content array depending on desired number of blocks
 
 var content = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 //var content = ["a", "a", "b", "b", "c", "c", "d", "d", "e", "e", "f", "f", "g", "g", "h", "h"];
 var contentLength = content.length;
 
-
-
-function createBoard() {
 
   for (i = 0; i < contentLength; i++) {
 
@@ -25,13 +31,7 @@ function createBoard() {
 
   }
 
-
-};
-
-createBoard();
-
-
-$(".card").click(function() {
+  $(".card").click(function() {
   $(this).addClass("flipped");
   clickz++;
   $(".counter").text("clicks:" +clickz);
@@ -42,16 +42,19 @@ $(".card").click(function() {
   if (count == 2) {
 
     $(".card").addClass("noClick");
-	
+  
     if (pair[0] == pair[1]) {
       //alert("match");
       $("." + pair[0]).addClass("matched");
+      audio.play();
       match++;
-	  $(".matches").text("matches:" + match);
+    $(".matches").text("matches:" + match);
       $(".card").removeClass("noClick");
       if (match == contentLength / 2) {
         $(".card").addClass("winner");
-	
+        audio2.play();
+
+  
       }
 
     } else {
@@ -68,13 +71,36 @@ $(".card").click(function() {
 
     pair = [];
     count = 0;
-	
-	
+  
+  
 
   }
 
 
 });
+
+
+
+};
+
+createBoard();
+
+function reset() {
+  $(".board").empty();
+  //count = 0;
+  //clickz = 0;
+  //match = 0;
+  createBoard();
+
+}
+
+$(document).ready(function(){
+    $(".reset").click(function(){
+        reset();
+    });
+});
+
+
 
 //$(".counter").click($(".card").css("fontFamily":"aliens"));
 
@@ -92,7 +118,7 @@ $(".aliens").click(function(){
  
  $(".space").click(function(){
 
-  $(".card").css("fontFamily","space");
+  $(".card").css({"fontFamily":"space"});
  }); 
 
  
